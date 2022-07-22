ea0f42a7c3ac9f2807413334a91ccbabdde3d849
kamal76k <109798859+kamal76k@users.noreply.github.com>
2022-07-22 20:21:52 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,62 @@
+# arithmetic operator in python 
+ 
+x=9
+y=3
+print(x+y)
+print(x-y) 
+print(x*y)
+print(x/y)
+
+# assignmenty operator
+
+x=5
+print(x)
+
+y=5
+y+=3
+print(y)
+
+# comparison operator 
+
+x=5
+y=5
+print(x==y)
+
+x=5
+y=4
+print(x!=y)
+
+x=5
+y=5
+print(x!=y)
+
+x=5
+y=4
+print(x>y)
+
+# x greater than y
+x=5
+y=8
+print(x>y)
+
+# x less than y
+x=6
+y=8
+print(x<y)
+
+# logical operator in python 
+
+# and operator
+# it means both condition will be true
+x=7
+print(x>3 and x<8)
+
+# or operator 
+# it means only one condition will be true
+x=5
+print(x>3 or x<4)
+
+# not operator 
+
+x=5
+print(not(x>3) and x<10)
